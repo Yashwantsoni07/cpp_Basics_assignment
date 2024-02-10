f7c451e57310bf88f05eddd8d1de08dd5904bcb8
Yashwantsoni07 <137249229+Yashwantsoni07@users.noreply.github.com>
2024-02-10 16:11:13 +0530

    solved assignment files are added.

---
+++ C++ basics assignment/cicumferenceOfCircle.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float radius = 10;
+    float pi = 3.1415;
+    float circum = 2 * pi * radius;
+    cout <<"Circumference of circle is : "<<circum;
+}
